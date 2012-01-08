53a75c6a3188e7de2f44c47c9c85ce2c76e7dba9
Dan F-M <dan@danfm.ca>
2012-01-08 14:52:40 -0500

    adding setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysdss',
+        version='0',
+        author='Camp Hogg',
+        packages=['sdss', 'sdss.astro'],
+        )
+
